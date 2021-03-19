97fdc6973d42a55cfa5733783d35c1ba3d388db8
GuessCode <48260689+Oumaimafisaoui@users.noreply.github.com>
2021-03-19 20:54:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cart:[],
+            premium: true
+        }
+            
+    },
+       methods:  {
+           updateCart(id){
+               this.cart.push(id)
+           }
+       
+        }
+       
+})
+
